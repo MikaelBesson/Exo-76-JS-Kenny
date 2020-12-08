aa00f2ed3c264f656ae67c09f9598e439d38ebbd
BessonMikael <besson.mikael04@gmail.com>
2020-12-08 15:20:00 +0100

    kenny is alive

--- script.js
+++ script.js
@@ -1,7 +1,61 @@
-
+let kenny = document.getElementById("kenny");
+let mortDeKenny =0;
 
 document.getElementById('up').addEventListener('click',function() {
-    let t = parseInt(document.getElementById('kenny').style.top);
-    t = t - 10;
-    document.getElementById('kenny').style.top = t + 'px';
+    let t = parseInt(kenny.style.top);
+    if(t > 0){
+        t = t - 10;
+        kenny.style.top = t + 'px';
+    }
 });
+
+document.getElementById("left").addEventListener("click", function (){
+    let t = parseInt(kenny.style.left);
+    if(t > 0){
+        t = t - 10;
+        kenny.style.left = t + "px";
+    }
+});
+
+document.getElementById("right").addEventListener("click", function (){
+    let t = parseInt(kenny.style.left);
+    if(t < 470){
+        t = t + 10;
+        kenny.style.left = t + "px";
+    }
+});
+
+document.getElementById("down").addEventListener("click", function (){
+    let t = parseInt(kenny.style.top);
+    if(t < 470){
+        t = t + 10;
+        kenny.style.top = t + "px";
+    }
+})
+let wiew = document.getElementById("viewport");
+
+let zoneRouge =document.createElement("div");
+zoneRouge.style.width = "50px";
+zoneRouge.style.height ="50px";
+zoneRouge.style.backgroundColor = "red";
+
+wiew.append(zoneRouge);
+
+
+document.querySelector("#up,#left").addEventListener('click',function (){
+    let t1 = parseInt(kenny.style.top);
+    let t2 = parseInt(kenny.style.left);
+    if(t1 < 50 && t2 < 50){
+        alert("kenny est mort ");
+        mortDeKenny +=1;
+        return mortDeKenny;
+    }
+})
+
+let dead = document.getElementById("deadKenny");
+dead.innerHTML = dead.innerHTML +  "kenny est mort " + mortDeKenny + " fois";
+
+
+
+
+
