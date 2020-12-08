4e965661b330658915a11e4a5afd2795392cfb6c
Besson Mikael <73292500+MikaelBesson@users.noreply.github.com>
2020-12-08 20:29:08 +0100

    kenny fini

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let kenny = document.getElementById("kenny");
 let mortDeKenny =0;
 
+    //deplacer kenny vers le haut
+
 document.getElementById('up').addEventListener('click',function() {
     let t = parseInt(kenny.style.top);
     if(t > 0){
@@ -8,6 +10,7 @@ document.getElementById('up').addEventListener('click',function() {
         kenny.style.top = t + 'px';
     }
 });
+    //deplacer kenny vers la gauche
 
 document.getElementById("left").addEventListener("click", function (){
     let t = parseInt(kenny.style.left);
@@ -15,7 +18,9 @@ document.getElementById("left").addEventListener("click", function (){
         t = t - 10;
         kenny.style.left = t + "px";
     }
+
 });
+    //deplacer kenny vers la droite
 
 document.getElementById("right").addEventListener("click", function (){
     let t = parseInt(kenny.style.left);
@@ -24,6 +29,7 @@ document.getElementById("right").addEventListener("click", function (){
         kenny.style.left = t + "px";
     }
 });
+    //deplacer kenny vers le bas
 
 document.getElementById("down").addEventListener("click", function (){
     let t = parseInt(kenny.style.top);
@@ -32,28 +38,122 @@ document.getElementById("down").addEventListener("click", function (){
         kenny.style.top = t + "px";
     }
 })
-let wiew = document.getElementById("viewport");
+    //creation de la zone rouge 1
 
-let zoneRouge =document.createElement("div");
-zoneRouge.style.width = "50px";
-zoneRouge.style.height ="50px";
-zoneRouge.style.backgroundColor = "red";
+let zoneRouge1 =document.createElement("div");
+zoneRouge1.style.width = "50px";
+zoneRouge1.style.height ="500px";
+zoneRouge1.style.backgroundColor = "red";
+    //ajout de la zone rouge 1
+document.getElementById("viewport").append(zoneRouge1);
 
-wiew.append(zoneRouge);
+    //creation de la zone rouge 2
+
+let zonerouge2 = document.createElement("div");
+zonerouge2.style.width = "50px";
+zonerouge2.style.height ="500px";
+zonerouge2.style.backgroundColor = "red";
+zonerouge2.style.marginLeft = "450px";
+zonerouge2.style.marginTop = "-500px";
+
+    //ajout de la zone rouge 2
+document.getElementById("viewport").append(zonerouge2);
 
 
-document.querySelector("#up,#left").addEventListener('click',function (){
-    let t1 = parseInt(kenny.style.top);
+    //limite de la zone rouge qui tueras kenny
+
+document.getElementById("left").addEventListener('click',function (){
     let t2 = parseInt(kenny.style.left);
-    if(t1 < 50 && t2 < 50){
+    if(t2 < 50) {
+        kenny.style.left = 200 + "px";
+        kenny.style.top = 200 + "px";
         alert("kenny est mort ");
-        mortDeKenny +=1;
-        return mortDeKenny;
+        mortDeKenny ++;
+        dead.innerHTML = "kenny est mort " + mortDeKenny + "fois";
+
+    }
+});
+document.getElementById("right").addEventListener('click',function () {
+    let t2 = parseInt(kenny.style.left);
+    if (t2 > 420) {
+        kenny.style.left = 200 + "px";
+        kenny.style.top = 200 + "px";
+        alert("kenny est mort ");
+        mortDeKenny ++;
+        dead.innerHTML = "kenny est mort " + mortDeKenny + "fois";
+
+    }
+});
+let dead = document.getElementById("deadKenny");
+dead.innerHTML = "kenny est mort " + mortDeKenny + "fois";
+
+
+
+//deplacement par les touches
+let body = document.body;
+body.addEventListener("keydown", function (event){
+    if(event.key === "ArrowUp"){
+        let t =parseInt(kenny.style.top);
+        if(t > 0){
+            t = t - 10;
+            kenny.style.top = t + 'px';
+        }
+    }
+})
+body.addEventListener("keydown", function (event){
+    if(event.key === "ArrowDown"){
+        let t =parseInt(kenny.style.top);
+        if(t < 470){
+            t = t + 10;
+            kenny.style.top = t + 'px';
+        }
+    }
+})
+body.addEventListener("keydown", function (event){
+    if(event.key === "ArrowLeft"){
+        let t =parseInt(kenny.style.left);
+        if(t > 0){
+            t = t - 10;
+            kenny.style.left = t + 'px';
+        }
+        if(t < 50) {
+            kenny.style.left = 200 + "px";
+            kenny.style.top = 200 + "px";
+            alert("kenny est mort ");
+            mortDeKenny ++;
+            dead.innerHTML = "kenny est mort " + mortDeKenny + "fois";
+
+        }
+    }
+})
+body.addEventListener("keydown", function (event){
+    if(event.key === "ArrowRight"){
+        let t =parseInt(kenny.style.left);
+        if(t < 470){
+            t = t + 10;
+            kenny.style.left = t + 'px';
+        }
+        if(t > 420) {
+            kenny.style.left = 200 + "px";
+            kenny.style.top = 200 + "px";
+            alert("kenny est mort ");
+            mortDeKenny ++;
+            dead.innerHTML = "kenny est mort " + mortDeKenny + "fois";
+
+        }
     }
 })
 
-let dead = document.getElementById("deadKenny");
-dead.innerHTML = dead.innerHTML +  "kenny est mort " + mortDeKenny + " fois";
+
+
+
+
+
+
+
+
+
+
 
 
 
